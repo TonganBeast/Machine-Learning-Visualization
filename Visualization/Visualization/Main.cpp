91f129ccb3bec12a31ba177a0e0c9d1e5fecba52
flubadubdub <flubadubdub@users.noreply.github.com>
2019-03-13 14:32:56 -0700

    Added GLUI and GLUT libraries

--- Visualization/Visualization/Main.cpp
+++ Visualization/Visualization/Main.cpp
@@ -28,17 +28,16 @@ int main(int argc, char** argv) {
 void dataFit(int numPlanes) {
 	std::cout << numPlanes << "\n";
 	int x = (numPlanes * 300) + 100;
-	//glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+	std::cout << x << "\n";
+	glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 	//gluOrtho2D(0.0, x, 0.0, SCREEN_HEIGHT);
 	glClear(GL_COLOR_BUFFER_BIT);
 }
 
 void Display() {
 	glClear(GL_COLOR_BUFFER_BIT);
-	//glViewport(0.0, 0.0, SCREEN_WIDTH, SCREEN_HEIGHT);
-	//gluOrtho2D(0.0, (graph.getGraph().getNumPlanes() * 300) + 100, 0.0, 400);
-	//dataFit(graph.getGraph().getNumPlanes());
-	graph.getGraph().buildGraph(true, true, true, false);
+	dataFit(graph.getGraph().getNumPlanes());
+	graph.getGraph().buildGraph(true, true, true, true);
 }
 
 void Init(void) {
@@ -46,7 +45,8 @@ void Init(void) {
 	glColor3f(0.0, 0.0, 0.0);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluOrtho2D(0.0, SCREEN_WIDTH, 0.0, SCREEN_HEIGHT);
+	glViewport(0, 0, 1000, 400);
+	gluOrtho2D(0.0, (graph.getGraph().getNumPlanes() * 300 + 100), 0.0, SCREEN_HEIGHT);
 	glClear(GL_COLOR_BUFFER_BIT);
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
